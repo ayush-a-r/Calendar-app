f0e29b2efcf291b4e0f076e286cd28f1901b7267
Ayush Ranjan <knight112619@gmail.com>
2025-11-16 21:56:54 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,12 @@ const { connect } = require("./db");
 const eventsRouter = require("./routes/events");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://calendar-app13.netlify.app",
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/events", eventsRouter);
